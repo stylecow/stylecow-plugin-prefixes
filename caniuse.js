f47a098f10eb1d276e2ac35dc0010942661a808e
oscarotero <oom@oscarotero.com>
2015-06-30 16:41:10 +0200

    fixed plugin to be more browserify-friendly

--- caniuse.js
+++ caniuse.js
@@ -1,32 +1,52 @@
-module.exports = {
-	forEachVendor: function (featName, strict, callback) {
-		var callbackArgs = Array.prototype.slice.call(arguments);
-		callbackArgs.shift();
-
-		if (typeof strict === 'boolean') {
-			callbackArgs.shift();
-		} else {
-			strict = false;
-		}
-
-		callback = callbackArgs[0];
-
-		getTaskInfo(featName, strict).forEach(function (taskInfo) {
-			callbackArgs[0] = taskInfo;
-			callback.apply(null, callbackArgs);
-		});
-	},
-	getTaskInfo: getTaskInfo,
-	read: read,
-	getVendorMinSupport: function (featName, vendor, strict) {
-		var taskInfo = getTaskInfo(featName, strict).filter(function (task) {
-			return task.vendor === vendor;
-		}).pop();
-
-		if (taskInfo) {
-			return taskInfo.browsers;
-		}
-	}
+var features = {
+	'background-img-opts': require('caniuse-db/features-json/background-img-opts.json'),
+	'border-image': require('caniuse-db/features-json/border-image.json'),
+	'border-radius': require('caniuse-db/features-json/border-radius.json'),
+	'calc': require('caniuse-db/features-json/calc.json'),
+	'css-animation': require('caniuse-db/features-json/css-animation.json'),
+	'css-appearance': require('caniuse-db/features-json/css-appearance.json'),
+	'css-boxdecorationbreak': require('caniuse-db/features-json/css-boxdecorationbreak.json'),
+	'css-boxshadow': require('caniuse-db/features-json/css-boxshadow.json'),
+	'css-clip-path': require('caniuse-db/features-json/css-clip-path.json'),
+	'css-crisp-edges': require('caniuse-db/features-json/css-crisp-edges.json'),
+	'css-deviceadaptation': require('caniuse-db/features-json/css-deviceadaptation.json'),
+	'css-exclusions': require('caniuse-db/features-json/css-exclusions.json'),
+	'css-filters': require('caniuse-db/features-json/css-filters.json'),
+	'css-gradients': require('caniuse-db/features-json/css-gradients.json'),
+	'css-grid': require('caniuse-db/features-json/css-grid.json'),
+	'css-hyphens': require('caniuse-db/features-json/css-hyphens.json'),
+	'css-image-set': require('caniuse-db/features-json/css-image-set.json'),
+	'css-logical-props': require('caniuse-db/features-json/css-logical-props.json'),
+	'css-masks': require('caniuse-db/features-json/css-masks.json'),
+	'css-placeholder': require('caniuse-db/features-json/css-placeholder.json'),
+	'css-regions': require('caniuse-db/features-json/css-regions.json'),
+	'css-repeating-gradients': require('caniuse-db/features-json/css-repeating-gradients.json'),
+	'css-resize': require('caniuse-db/features-json/css-resize.json'),
+	'css-selection': require('caniuse-db/features-json/css-selection.json'),
+	'css-shapes': require('caniuse-db/features-json/css-shapes.json'),
+	'css-snappoints': require('caniuse-db/features-json/css-snappoints.json'),
+	'css-sticky': require('caniuse-db/features-json/css-sticky.json'),
+	'css-text-align-last': require('caniuse-db/features-json/css-text-align-last.json'),
+	'css-touch-action': require('caniuse-db/features-json/css-touch-action.json'),
+	'css-transitions': require('caniuse-db/features-json/css-transitions.json'),
+	'css3-boxsizing': require('caniuse-db/features-json/css3-boxsizing.json'),
+	'css3-cursors-newer': require('caniuse-db/features-json/css3-cursors-newer.json'),
+	'css3-tabsize': require('caniuse-db/features-json/css3-tabsize.json'),
+	'flexbox': require('caniuse-db/features-json/flexbox.json'),
+	'font-feature': require('caniuse-db/features-json/font-feature.json'),
+	'font-kerning': require('caniuse-db/features-json/font-kerning.json'),
+	'fullscreen': require('caniuse-db/features-json/fullscreen.json'),
+	'inline-block': require('caniuse-db/features-json/inline-block.json'),
+	'intrinsic-width': require('caniuse-db/features-json/intrinsic-width.json'),
+	'multicolumn': require('caniuse-db/features-json/multicolumn.json'),
+	'object-fit': require('caniuse-db/features-json/object-fit.json'),
+	'text-decoration': require('caniuse-db/features-json/text-decoration.json'),
+	'text-emphasis': require('caniuse-db/features-json/text-emphasis.json'),
+	'text-overflow': require('caniuse-db/features-json/text-overflow.json'),
+	'text-size-adjust': require('caniuse-db/features-json/text-size-adjust.json'),
+	'transforms2d': require('caniuse-db/features-json/transforms2d.json'),
+	'transforms3d': require('caniuse-db/features-json/transforms3d.json'),
+	'user-select-none': require('caniuse-db/features-json/user-select-none.json')
 };
 
 var browsers = {
@@ -46,8 +66,49 @@ var vendors = [
 	['o', ['opera']]
 ];
 
+module.exports = {
+	data: features,
+	forEachVendor: function (featName, strict, callback) {
+		var callbackArgs = Array.prototype.slice.call(arguments);
+		callbackArgs.shift();
+
+		if (typeof strict === 'boolean') {
+			callbackArgs.shift();
+		} else {
+			strict = false;
+		}
+
+		callback = callbackArgs[0];
+
+		getTaskInfo(featName, strict).forEach(function (taskInfo) {
+			callbackArgs[0] = taskInfo;
+			callback.apply(null, callbackArgs);
+		});
+	},
+	getTaskInfo: getTaskInfo,
+	getVendorMinSupport: function (featName, vendor, strict) {
+		var taskInfo = getTaskInfo(featName, strict).filter(function (task) {
+			return task.vendor === vendor;
+		}).pop();
+
+		if (taskInfo) {
+			return taskInfo.browsers;
+		}
+	}
+};
+
 function getTaskInfo (featName, strict) {
-	var data = read(featName);
+	var data;
+
+	if (typeof featName === 'string') {
+		data = features[featName];
+
+		if (!data) {
+			throw new Error('"' + featName + '" is not valid');
+		}
+	} else {
+		data = featName;
+	}
 
 	return vendors
 		.map(function (vendor) {
@@ -89,10 +150,6 @@ function getTaskInfo (featName, strict) {
 		.filter(function (task) {
 			return task ? true : false;
 		});
-}
-
-function read (featName) {
-	return require('./node_modules/caniuse-db/features-json/' + featName + '.json');
 }
 
 function getMinSupportFromStat (stats, browser, vendor, strict) {
--- tests/caniuse-checker.js
+++ tests/caniuse-checker.js
@@ -1,82 +1,41 @@
 //Check the caniuse database to discover new css features
 var fs = require('fs');
 var caniuse = require(__dirname + '/../caniuse');
-var features = fs.readdirSync(__dirname + '/../node_modules/caniuse-db/features-json');
-
-var implemented = [
-	'background-img-opts',
-	'border-image',
-	'border-radius',
-	'calc',
-	'css-animation',
-	'css-appearance',
-	'css-boxdecorationbreak',
-	'css-boxshadow',
-	'css-canvas', //not implemented
-	'css-clip-path',
-	'css-crisp-edges',
-	'css-deviceadaptation',
-	'css-exclusions',
-	'css-filters',
-	'css-gradients',
-	'css-grid',
-	'css-hyphens',
-	'css-image-set',
-	'css-logical-props',
-	'css-masks',
+var featuresDir = __dirname + '/../node_modules/caniuse-db/features-json';
+var notImplemented = [
+	'css-canvas',
 	'css-media-resolution', //implemented in stylecow-plugin-fixes
-	'css-placeholder',
-	'css-reflections', //not implemented
-	'css-regions',
-	'css-repeating-gradients',
-	'css-resize',
-	'css-selection',
-	'css-shapes',
-	'css-snappoints',
-	'css-sticky',
-	'css-text-align-last',
-	'css-touch-action',
-	'css-transitions',
-	'css3-boxsizing',
-	'css3-cursors-newer',
-	'css3-tabsize',
-	'flexbox',
-	'font-feature',
-	'font-kerning',
-	'font-smooth', //not implemented
-	'inline-block',
-	'intrinsic-width',
-	'multicolumn',
-	'object-fit',
-	'text-decoration',
-	'text-emphasis',
-	'text-overflow',
-	'text-size-adjust',
-	'text-stroke', //not implemented
-	'transforms2d',
-	'transforms3d',
-	'user-select-none'
+	'css-reflections',
+	'font-smooth',
+	'text-stroke',
 ];
 
-features.forEach(function (featName) {
+fs.readdirSync(featuresDir).forEach(function (featName) {
+	var featData = require(featuresDir + '/' + featName);
 	featName = featName.replace('.json', '');
 
-	if (implemented.indexOf(featName) !== -1) {
+	// is in not-implemented list?
+	if (notImplemented.indexOf(featName) !== -1) {
 		return;
 	}
 
-	var categories = caniuse.read(featName).categories;
-
+	// is not about css
 	if (
-		   categories.indexOf('CSS') === -1
-		&& categories.indexOf('CSS2') === -1
-		&& categories.indexOf('CSS3') === -1
+		   featData.categories.indexOf('CSS') === -1
+		&& featData.categories.indexOf('CSS2') === -1
+		&& featData.categories.indexOf('CSS3') === -1
 	) {
 		return;
 	}
 
-	var support = caniuse.getTaskInfo(featName);
+	// is implemented already
+	if (caniuse.data[featName]) {
+		return;
+	}
 
+	var support = caniuse.getTaskInfo(featData);
+
+	//need support?
 	if (support.length) {
 		console.log('---');
 		console.log(featName);
