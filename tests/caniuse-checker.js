9b3c7f795864096234a52655961c11c5c99c99a4
oscarotero <oom@oscarotero.com>
2016-03-24 13:20:42 +0100

    ignored new non-standard css properties

--- tests/caniuse-checker.js
+++ tests/caniuse-checker.js
@@ -3,11 +3,13 @@ var fs = require('fs');
 var caniuse = require(__dirname + '/../caniuse');
 var featuresDir = __dirname + '/../node_modules/caniuse-db/features-json';
 var notImplemented = [
-	'css-canvas',
+	'css-canvas',           //non standard
 	'css-media-resolution', //implemented in stylecow-plugin-fixes
-	'css-reflections',
-	'font-smooth',
-	'text-stroke',
+	'css-reflections',      //non standard
+	'css-matches-pseudo',   //fallback in stylecow-plugin-matches
+	'font-smooth',          //non standard
+	'css-scrollbar',        //non standard
+	'text-stroke',          //non standard
 ];
 
 fs.readdirSync(featuresDir).forEach(function (featName) {
